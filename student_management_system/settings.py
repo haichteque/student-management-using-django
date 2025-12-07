219a71d4c24626587a37ff21170567c7e467f709
haichteque <huzaifatahirqazi@gmail.com>
2025-12-07 15:16:36 +0500

    Sentry plase work

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -172,10 +172,14 @@ prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 if os.environ.get('SENTRY_DSN'):
     sentry_sdk.init(
         dsn=os.environ.get('SENTRY_DSN'),
+        integrations=[
+            DjangoIntegration(),
+        ],
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         traces_sample_rate=1.0,
@@ -183,4 +187,7 @@ if os.environ.get('SENTRY_DSN'):
         # of sampled transactions.
         # We recommend adjusting this value in production.
         profiles_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
     )
